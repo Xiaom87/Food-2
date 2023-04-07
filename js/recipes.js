2667c7241aeb692a2c7c483802299c719be657dd
Xiaom87 <teplecov1987@gmail.com>
2023-04-07 22:05:25 +0400

    добавил footer

--- js/recipes.js
+++ js/recipes.js
@@ -5,7 +5,7 @@ const tabHeader = document.querySelectorAll('[data-tab]');
 tabHeader.forEach(function(item) {
    item.addEventListener('click', function () { 
 
-      //ataset позволяет считывать или устанавливать любые дата-атрибуты на HTML-элементе. Дата-атрибут — это пользовательский атрибут на элементе, название которого начинается с data- , например data-testid . Дата атрибуты используются, чтобы хранить значения на элементах в HTML.
+      //dataset позволяет считывать или устанавливать любые дата-атрибуты на HTML-элементе. Дата-атрибут — это пользовательский атрибут на элементе, название которого начинается с data- , например data-testid . Дата атрибуты используются, чтобы хранить значения на элементах в HTML.
      
       const contentBox = document.querySelector('#' + this.dataset.tab)
       console.log(contentBox)
