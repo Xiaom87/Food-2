c9f91516996fbeec830c3a7aaf85c0aa418923ef
Xiaom87 <teplecov1987@gmail.com>
2023-04-07 16:57:14 +0400

    добавил страницу рецепты

---
+++ js/recipes.js
@@ -0,0 +1,19 @@
+/*---------------------------tabs start---------------------------------- */
+// нашли все заголовки табов по дата отрибуту ('[data-tab]')
+const tabHeader = document.querySelectorAll('[data-tab]');
+
+tabHeader.forEach(function(item) {
+   item.addEventListener('click', function () { 
+
+      //ataset позволяет считывать или устанавливать любые дата-атрибуты на HTML-элементе. Дата-атрибут — это пользовательский атрибут на элементе, название которого начинается с data- , например data-testid . Дата атрибуты используются, чтобы хранить значения на элементах в HTML.
+     
+      const contentBox = document.querySelector('#' + this.dataset.tab)
+      console.log(contentBox)
+      contentBox.classList.toggle('hidden')
+    })
+});
+
+
+
+
+/*---------------------------tabs end------------------------------------ */
